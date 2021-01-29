9ef0abe0aa98da220ac4c387316561a3e6ec75b8
MelodyLayne <melodylayne79@gmail.com>
2021-01-29 10:39:36 -0600

    start of day

---
+++ Module4/Week12/Resources/jwt_auth/routes/users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const bearerToken = require('express-bearer-token');
+const { User } = require('../models');
+const { generateUserToken, requireAuth } = require('../auth');
+const router = express.Router();
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+router.use(express.json());
+router.use(bearerToken());
+
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    // create a new user
+    // generate a token to send in our response
+  })
+);
+
+// create a protected route
+
+module.exports = router;

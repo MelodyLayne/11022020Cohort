6d31e29a9181118ac1ef2ef5fd992349fd929281
MelodyLayne <melodylayne79@gmail.com>
2021-02-15 14:27:07 -0600

    day one codealong set up

---
+++ Module5/Week15/CodeAlong/classes-starter/src/components/Child.js
@@ -0,0 +1,11 @@
+function Child({ count, message }) {
+  return (
+    <>
+      <h1>Hello from child!</h1>
+      <h2>Count: {count}</h2>
+      <h2>Message: {message}</h2>
+    </>
+  );
+}
+
+export default Child;

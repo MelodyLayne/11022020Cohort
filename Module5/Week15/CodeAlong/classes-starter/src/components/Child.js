709910711198eaea8c9ebd6b41fe690de3d95850
MelodyLayne <melodylayne79@gmail.com>
2021-02-15 15:35:09 -0600

    refactor class components

--- Module5/Week15/CodeAlong/classes-starter/src/components/Child.js
+++ Module5/Week15/CodeAlong/classes-starter/src/components/Child.js
@@ -1,11 +1,28 @@
-function Child({ count, message }) {
-  return (
-    <>
-      <h1>Hello from child!</h1>
-      <h2>Count: {count}</h2>
-      <h2>Message: {message}</h2>
-    </>
-  );
+// function Child({ count, message }) {
+  // return (
+  //   <>
+  //     <h1>Hello from child!</h1>
+  //     <h2>Count: {count}</h2>
+  //     <h2>Message: {message}</h2>
+  //   </>
+//   );
+// }
+
+
+import { Component } from 'react'
+
+class Child extends Component {
+  // const { count, message } = this.props
+  // destructuring props will allow you to not have to do the 'this.props' arguments in the return statements.
+
+  render() {
+    return (
+      <>
+        <h1>Hello from child!</h1>
+        <h2>Count: {this.props.count}</h2>
+        <h2>Message: {this.props.message}</h2>
+      </>
+  }
 }
 
 export default Child;
--- Module5/Week15/CodeAlong/classes-starter/src/components/Parent.js
+++ Module5/Week15/CodeAlong/classes-starter/src/components/Parent.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+/* import { useState, useEffect } from "react";
 import Child from "./Child";
 
 function Parent() {
@@ -8,6 +8,7 @@ function Parent() {
   useEffect(() => {
     console.log("I have mounted");
     return () => {
+      console.log("I have mounted");
       console.log("I am going to unmount");
     };
   }, []);
@@ -26,13 +27,70 @@ function Parent() {
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>Increment</button>
       <button onClick={() => setCount((prevCount) => prevCount - 1)}>Decrement</button>
       <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
+      type="text"
+      value={message}
+      onChange={(e) => setMessage(e.target.value)}
       />
       <Child count={count} message={message} />
     </>
   );
+}
+
+export default Parent;
+*/
+
+// refactoring the above code for classes.
+
+import React from 'react';
+import Child from './Child';
+
+class Parent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      message: '',
+    }
+  }
+
+  componentDidMount() {
+    console.log("I have mounted");
+  }
+
+  componentDidUpdate() {
+    console.log('State was updated to '+ this.state)
+  }
+  componentWillUnmount() {
+    console.log("I am going to unmount");
+
+  }
+
+  render() {
+    return (
+    <>
+      <h1>Hello from parent!</h1>
+        <button
+          onClick={() =>
+            this.setState((prevState => ({
+        count: prevState.count + 1,
+            }))
+    }>
+          Increment
+    </button>
+      <button onClick={() => this.setState((prevState => ({
+        count: prevState.count - 1
+      }))}>Decrement</button>
+      <input
+        type="text"
+        value={this.state.message}
+          onChange={(e) => this.setState({
+            message: e.target.value
+          })}
+      />
+      <Child count={this.state.count} message={this.state.message} />
+    </>
+    )
+  }
 }
 
 export default Parent;

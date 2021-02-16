d919a7d39fb209ec98c796270ed40ad79a6aa634
MelodyLayne <melodylayne79@gmail.com>
2021-02-15 18:06:05 -0600

    finish phase 2

--- Module5/Week15/Exercises/Day1/refactor-widgets-master/src/components/Auto.js
+++ Module5/Week15/Exercises/Day1/refactor-widgets-master/src/components/Auto.js
@@ -2,67 +2,54 @@ import { useState } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 function AutoComplete({ names }) {
-  const [inputVal, setInputVal] = useState('');
+	const [inputVal, setInputVal] = useState('');
 
+	const handleInput = (e) => {
+	setInputVal(e.target.value);
+	};
 
-  handleInput = (e) => {
-    this.setState({ inputVal: e.target.value });
-  }
+	const selectName = (e) => {
+		const name = e.target.innerText;
+		setInputVal(name);
+	};
 
-  selectName = (e) => {
-    const name = e.target.innerText;
-    this.setState({ inputVal: name });
-  }
+	const matches = () => {
+		const inputLength = inputVal.length;
+		const matches = [];
 
-  matches = () => {
-    const { inputVal } = this.state;
-    const { names } = this.props;
-    const inputLength = inputVal.length;
-    const matches = [];
+		if (inputLength === 0) return names;
 
-    if (inputLength === 0) return names;
+		names.forEach((name) => {
+			const nameSegment = name.slice(0, inputLength);
+			if (nameSegment.toLowerCase() === inputVal.toLowerCase()) {
+				matches.push(name);
+			}
+		});
 
-    names.forEach(name => {
-      const nameSegment = name.slice(0, inputLength);
-      if (nameSegment.toLowerCase() === inputVal.toLowerCase()) {
-        matches.push(name);
-      }
-    });
+		if (matches.length === 0) matches.push('No matches');
 
-    if (matches.length === 0) matches.push('No matches');
+		return matches;
+	};
 
-    return matches;
-  }
+	const results = matches().map((result) => (
+		<CSSTransition key={result} classNames="result" timeout={{ enter: 500, exit: 300 }}>
+			<li>{result}</li>
+		</CSSTransition>
+	));
 
-  render() {
-    const results = this.matches().map((result) => (
-      <CSSTransition
-        key={result}
-        classNames="result"
-        timeout={{ enter: 500, exit: 300 }}
-      >
-        <li>{result}</li>
-      </CSSTransition>
-    ));
-
-    return (
-      <section>
-        <h1>Autocomplete</h1>
-        <div className="auto">
-          <input
-            onChange={this.handleInput}
-            value={this.state.inputVal}
-            placeholder="Search..."
-          />
-          <ul className="auto-dropdown" onClick={this.selectName}>
-            <TransitionGroup>
-              {results}
-            </TransitionGroup>
-          </ul>
-        </div>
-      </section>
-    );
-  }
-};
+	return (
+		<section>
+			<h1>Autocomplete</h1>
+			<div className="auto">
+				<input onChange={handleInput} value={inputVal} placeholder="Search..." />
+				<ul className="auto-dropdown" onClick={selectName}>
+          <TransitionGroup>
+            {results}
+          </TransitionGroup>
+				</ul>
+			</div>
+		</section>
+	);
+}
 
 export default AutoComplete;
